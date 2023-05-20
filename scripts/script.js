92b601520adbf701f02266fd0c4fca44d607459c
Trish Daugherty <124315248+trishFEWD2@users.noreply.github.com>
2023-05-20 13:14:46 -0700

    Add files via upload
    added all project files including minified - need to change those links on pages

---
+++ scripts/script.js
@@ -0,0 +1,61 @@
+/////////////////////////////////////
+// hamburger menu transformation and show'hide
+/////////////////////////////////////
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}))
+
+
+/////////////////////////////////////
+// carousel script 
+/////////////////////////////////////
+const slidesContainer = document.getElementById("slides-container");
+const slide = document.querySelector(".slide");
+const prevButton = document.getElementById("slide-arrow-prev");
+const nextButton = document.getElementById("slide-arrow-next");
+
+nextButton.addEventListener("click", (event) => {
+  const slideWidth = slide.clientWidth;
+    slidesContainer.scrollLeft += slideWidth;
+});
+
+prevButton.addEventListener("click", () => {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft -= slideWidth;
+});
+
+
+/////////////////////////////////////
+// contact form script
+/////////////////////////////////////
+const formHeading = document.querySelector(".form-heading");
+const formInputs = document.querySelectorAll(".contact-form-input");
+
+formInputs.forEach((input) => {
+  input.addEventListener("focus", () => {
+    formHeading.style.opacity = "0";
+    setTimeout(() => {
+      formHeading.textContent = `Your ${input.placeholder}`;
+      formHeading.style.opacity = "1";
+    }, 300);
+  });
+
+  input.addEventListener("blur", () => {
+    formHeading.style.opacity = "0";
+    setTimeout(() => {
+      formHeading.textContent = "Let's Talk";
+      formHeading.style.opacity = "1";
+    }, 300);
+  });
+});
+
